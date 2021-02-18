2b5f7847f73d6fff6b04ff3bf359cd756b01160f
pshoben <pshoben@yahoo.com>
2021-02-18 21:16:48 +0000

    affinity - wip

--- benchmark_record_results.sh
+++ benchmark_record_results.sh
@@ -1,2 +1,3 @@
-./run_benchmark_v1.sh | tee benchmark_v1_results_date_`date +%y%m%d_%h%m%s`.txt
+#./run_benchmark_v1.sh | tee benchmark_v1_results_date_`date +%y%m%d_%h%m%s`.txt
+./run_benchmark_v2.sh | tee benchmark_v2_results_date_`date +%y%m%d_%h%m%s`.txt
 
--- threads/queue_wrapper.h
+++ threads/queue_wrapper.h
@@ -1,64 +1,82 @@
 #pragma once
 
 #include <queue>
-//#include <list>
-//#include <stdint.h>
 #include <mutex>
-//#include <vector>
 #include <cstring>
 #include "common.h"
 
 struct QueueWrapper {
 public:
-  std::queue<struct timespec> queue;
-  std::mutex queue_mutex;
+ std::queue<struct timespec> queue;
+ std::mutex queue_mutex;
 
-  static const size_t MAX_SAMPLES = 1000000U;
-  //std::vector<TimespecPair> samples{ MAX_SAMPLES };
+ static const size_t MAX_SAMPLES = 1000000U;
 
- struct timespec write_v1() {
+ struct timespec write_v1() 
+ {
     std::unique_lock<std::mutex> guard( queue_mutex );
     struct timespec write_time;
     clock_gettime( CLOCK_MONOTONIC, &write_time );
-    //std::cout << "writer got time : " << write_time.tv_sec << "." << write_time.tv_nsec << "\n";
-    //uint64_t val=(uint64_t) write_time.tv_nsec;
     queue.push( write_time );
     return write_time;
  }
 
- TimespecPair * read_v1( TimespecPair * next_sample ) {
+ TimespecPair * read_v1( TimespecPair * next_sample ) 
+ {
    std::unique_lock<std::mutex> guard( queue_mutex );
    if( !queue.empty()) {
      struct timespec write_time = queue.front();
      queue.pop();
+
      struct timespec read_time;
      clock_gettime( CLOCK_MONOTONIC, &read_time );
-     //std::cout << "reader got write time : " << write_time.tv_sec << "." << write_time.tv_nsec << "\n";
-     //std::cout << "reader got time : " << read_time.tv_sec << "." << read_time.tv_nsec << "\n";
- 
-     //samples.emplace_back( write_time, read_time ); 
 
-     // record the raw sample
      next_sample->write_time = write_time;
      next_sample->read_time = read_time;
-
-     // increment only after store
      next_sample++;
-
-     //int64_t queue_latency_ns = timespec_diff_ns( write_time, read_time );
-     //std::cout << " queue latency (ns) = " << queue_latency_ns << "\n";
    }
    return next_sample;
  } 
 
+ struct timespec write_v2() 
+ {
+    queue_mutex.lock();
+
+    struct timespec write_time;
+    clock_gettime( CLOCK_MONOTONIC, &write_time );
+    queue.push( write_time );
+    queue_mutex.unlock();
+    return write_time;
+ }
+
+ TimespecPair * read_v2( TimespecPair * next_sample ) 
+ {
+   queue_mutex.lock();
+   if( !queue.empty()) {
+
+     next_sample->write_time = queue.front();
+     queue.pop();
+     queue_mutex.unlock();
+
+     clock_gettime( CLOCK_MONOTONIC, &next_sample->read_time );
+     next_sample++;
+
+   } else {
+     queue_mutex.unlock();
+   }
+   return next_sample;
+ } 
+
+ //
+
  struct timespec write() 
  {
-     return write_v1();
+     return write_v2();
  } 
 
  TimespecPair * read( TimespecPair * next_sample ) 
  {
-     return read_v1( next_sample );
+     return read_v2( next_sample );
  }
 };
 

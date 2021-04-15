727ebc7829d5473aef192784eb1a6047886c06c4
pshoben <pshoben@yahoo.com>
2021-04-15 22:01:35 +0000

    shared mem exp

--- prot/fill.cpp
+++ prot/fill.cpp
@@ -1,95 +1,19 @@
 #include <stdio.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
 #include <thread>
 #include <chrono>
 
 #include "seg.h"
 
-#define STORAGE_ID "/SHM_TEST"
-#define STORAGE_SIZE SEGMENT_SIZE
-#define DATA "Hello, World! From PID %d"
-
 int main() // int argc, char *argv[])
 {
-	int res;
-	int fd;
-	//int len;
-	pid_t pid;
-	void *addr;
-	char data[STORAGE_SIZE];
+  Appendable a{};
 
-	pid = getpid();
-	sprintf(data, DATA, pid);
-
-	// get shared memory file descriptor (NOT a file) - must already exist
-	fd = shm_open(STORAGE_ID, O_RDWR, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-	{
-		perror("open");
-		return 10;
-	}
-
-//	// extend shared memory object as by default it's initialized with size 0
-//	res = ftruncate(fd, STORAGE_SIZE);
-//	if (res == -1)
-//	{
-//		perror("ftruncate");
-//		return 20;
-//	}
-
-	// map shared memory to process address space
-	addr = mmap(NULL, STORAGE_SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
-	if (addr == MAP_FAILED)
-	{
-		perror("mmap");
-		return 30;
-	}
-
-  SegHeader * header = (SegHeader *)addr;
-  header->print();
-
-  char * pfill = ((char*)addr); //  + header->start_offset;
-  uint32_t alloc_size = header->alloc_size;
-  uint32_t next_free_offset = header->next_free_offset;
-  while( next_free_offset < alloc_size ) {
-      printf("writing byte at %p offset %lu\n",pfill, pfill - ((char*)addr)); 
-      *pfill++ = 'x'; // write a byte
-      // increment the next free offset
-      next_free_offset++;
-      // push the value to shared memory with write barrier. assumes this thread is the only writer.
-      // release semantics : readers that load/acquire next_free_offset are guaranteed to see all earlier writes from here
-      __atomic_store_n( &( header->next_free_offset ), next_free_offset, __ATOMIC_RELEASE );
-
-      header->print(); 
-      // wait for a bit
-      std::this_thread::sleep_for( std::chrono::seconds( 1 ));
-  } 
-
-//	// place data into memory
-//	len = strlen(data) + 1;
-//	memcpy(addr, data, len);
-//
-//	// wait for someone to read it
-//	sleep(2);
-
-	// mmap cleanup
-	res = munmap(addr, STORAGE_SIZE);
-	if (res == -1)
-	{
-		perror("munmap");
-		return 40;
-	}
-
-//	// shm_open cleanup
-//	fd = shm_unlink(STORAGE_ID);
-//	if (fd == -1)
-//	{
-//		perror("unlink");
-//		return 100;
-//	}
-
+  while( true ) {
+      a.append('x');
+      a.print();
+      std::this_thread::sleep_for( std::chrono::milliseconds( 1000 ));
+  }
+  
 	return 0;
 }
--- prot/seg.h
+++ prot/seg.h
@@ -1,6 +1,9 @@
 #ifndef LLEXP_PROT_SEG_H
 #define LLEXP_PROT_SEG_H
 
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdint.h>
 #include <iostream>
 
@@ -25,33 +28,19 @@ public:
     bool operator==( Address const & other ) const {
         return ( segment == other.segment ) && ( offset == other.offset );
     }
+    void print() {
+        cout << " Address{ " << segment << ", " << offset << " }\n";
+    }
 
 }; // sizeof = 64 
 constexpr Address NULL_ADDRESS{0,0};
-
-//class ColumnMapping 
-//{
-//public:
-//    uint32_t col_id; // unique id of the column
-//    uint32_t segment_number; // segment number of Column struct
-//    uint32_t block_offset; // offset from start of segment of the start of the block
-//    char name[20]; // /unique name of the column
-//    void print( int i ) {
-//        cout << "ColumnMapping[" << i << "] = { " << col_id << " , " << segment_number << " , " << block_offset << " , " << name << "}\n";
-//    }
-//}; // sizeof = 32
 
 // SegHeader contains the mapping of column names to columns, each of which must be in this segment ( TODO : may be in this segment or another )
 class SegHeader 
 {
 public:
     uint32_t alloc_size=SEGMENT_SIZE; // the size of the segment in bytes
-    //uint32_t start_offset=0; // the first byte to be written to
     uint32_t next_free_offset= BLOCK_SIZE; // the first unwritten byte - skip the first block used for this SegHeader
-    //uint32_t num_columns=0; // size of columns array
-
-    //ColumnMapping column_mappings[MAX_COLUMNS];
-    // pad to full block
     char padding[ BLOCK_SIZE - ( sizeof( alloc_size )/* + sizeof( start_offset )*/ + sizeof( next_free_offset ))]; //  + sizeof( column_mappings ) + sizeof( num_columns ))];
 
     static SegHeader * instance()
@@ -75,18 +64,6 @@ public:
         }
         return top;
     }
-//    // one writer thread
-//    // return UINT32_MAX on fail 
-//    uint32_t alloc_block() {
-//        uint32_t alloc_offset = next_free_offset;
-//        if( next_free_offset + BLOCK_SIZE >= alloc_size ) {
-//            // out of space
-//            return UINT32_MAX;
-//        } else {
-//          __atomic_store_n( &next_free_offset, next_free_offset + BLOCK_SIZE, __ATOMIC_RELEASE );
-//          return alloc_offset;
-//        }
-//    } 
     // one writer thread
     // return Address{0,0} on fail
     Address alloc_block() {
@@ -96,72 +73,17 @@ public:
             return NULL_ADDRESS;
         } else {
             __atomic_store_n( &next_free_offset, next_free_offset + BLOCK_SIZE, __ATOMIC_RELEASE );
+            print();
             return Address{0,alloc_offset}; // just one segment
         }
     } 
 
     void print() 
     {
-        //auto start_addr = this;
-        //auto size = next_free_offset - start_offset;
         cout << "alloc_size: " << alloc_size << "\n" 
-             //<< "start_offset: " << start_offset << "\n" 
              << "next_free_offset: " << next_free_offset << "\n"; 
-             //<< "num_columns = " << num_columns << "\n";
-//        for( uint32_t i = 0 ; i < num_columns ; ++i ) {
-//            column_mappings[i].print(i);
-//            Column * col = get_column( i ) ;
-//            col->print();
-//        }
     }
-
-//    // get the column by index
-//    Column * get_column( uint32_t i ) 
-//    {
-//        // if the named column exists, return its address
-//        // if it doesn't exist, return null pointer
-//        if( i < num_columns ) {
-//             return (Column *)(char *)(((char*)this)+( column_mappings[i].block_offset ));
-//        }
-//        return nullptr;
-//    }
-//
-//    // get or create the named column
-//    Column * goc_column( const char * name ) 
-//    {
-//        // if the named column exists, return its address
-//        // if it doesn't exist, create it and return its address
-//        // if it can't be created, due to out of space, return null pointer
-//        uint32_t i = 0 ;
-//        while( i < num_columns ) {
-//            if( !strcmp( name, column_mappings[i].name )) {
-//                // found the column, return its address:
-//                return (Column *)(char *)(((char*)this)+( column_mappings[i].block_offset ));
-//            }
-//            i++;
-//        }
-//        if( i == MAX_COLUMNS ) {
-//            return nullptr;
-//        }
-//        // allocate the next free block in the segment
-//        uint32_t new_block_offset = alloc_block();
-//        if( new_block_offset == UINT32_MAX ) {
-//            // out of space
-//            return nullptr;
-//        }
-//        
-//        // create the column
-//        column_mappings[i]= ColumnMapping{ i, 0, new_block_offset , '\0' };
-//        strcpy( column_mappings[i].name, name );
-//        num_columns++;
-//        // placement new construct the column
-//        Column * addr = new ( ((char*)this ) + new_block_offset ) Column();
-//        //addr->print( i, addr );
-//        return addr;
-//    }
-     
 }; // sizeof = BLOCK_SIZE
-
 
 // appendable is an array of arrays
 class Appendable {
@@ -176,6 +98,7 @@ public:
     bool append( char c ) 
     {
         SegHeader * top = SegHeader::instance(); // assert valid 
+        top->print();
         if( addresses[ current_address_index ].offset >= BLOCK_SIZE )  { // [[ unlikely ]] 
             // current block full, take a new one
             if( current_address_index < ( MAX_ADDRESS )) { // [[ likely ]] 
@@ -200,6 +123,7 @@ public:
         uint32_t current = __atomic_load_n( &( current_address_index ), __ATOMIC_ACQUIRE );
         for( uint32_t i=0 ; i <= current ; ++i ) {
             Address block_address = addresses[ i ];
+            block_address.print();
             for( uint32_t j=0 ; j <= block_address.offset ; ++j ) {
                 char * p =  ((char *)top) + block_address.segment + j ;
                 cout << *p ;
@@ -208,62 +132,5 @@ public:
         cout << "\n";
     }
 }; // sizeof = BLOCK_SIZE
-
-
-
-//// temp : column is just a block
-//class Column {
-//public:
-//    uint32_t tag=0; // type info of the column
-//    uint32_t next_free_index=0; // the size of the array (in units of width)
-//    char data[ BLOCK_SIZE - ( sizeof( tag ) + sizeof( next_free_index ))];
-//
-//    void print( int i, Column * addr ) {
-//        cout << "Column[" << i << "] @" << (long)addr << " = { " << tag << " , " << next_free_index << "}\n";
-//    }
-//
-//    void append( char c ) {
-//        data[ next_free_index ] = c ;
-//        // single writer thread owns the column
-//        uint32_t next = next_free_index+1;
-//        __atomic_store_n( &next_free_index, next, __ATOMIC_RELEASE );
-//    } 
-//
-//    void print() {
-//        cout << "tag: " << tag << "\n" 
-//             << "next_free_index: " << next_free_index << "\n"; 
-//        for( uint32_t i=0 ; i < next_free_index ; ++i ) {
-//            cout << data[i];
-//        }
-//        cout << "\n";
-//    }
-//}; // sizeof = BLOCK_SIZE
-
-//class SegHeader {
-//public:
-//    uint32_t alloc_size;   // the size of the segment in bytes
-//    uint32_t next_free_offset; // the first unwritten byte
-//    char pad[ CACHELINE_SIZE - 3*sizeof( uint32_t ) ];// pad to full line
-//
-//    SegHeader() : alloc_size( SEGMENT_SIZE )
-//                //, start_offset( sizeof( SegHeader ))
-//                , next_free_offset( sizeof( SegHeader ))
-//                , pad{0}
-//    {}
-//  
-//    void print() {
-//        // print might be called from a reader thread
-//        uint32_t prev_free_offset = __atomic_load_n( &( next_free_offset ), __ATOMIC_ACQUIRE );
-//        cout << "alloc_size: " << alloc_size << "\n" 
-//             << "next_free_offset: " << prev_free_offset << "\n" 
-//             << "start address = " << start_addr << "\n";
-//        size_t offset = prev_start_offset;
-//        do {
-//           char * p = ((char *)this)+offset;
-//           cout << *p;
-//        } while ( offset++ < ( prev_free_offset ));
-//        cout << "\n";
-//    }
-//} ;
 
 #endif
